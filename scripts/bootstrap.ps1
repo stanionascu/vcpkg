f2b9263111b6b80593a6bae179255b2706623a8e
Alexander Karatarakis <alkarata@microsoft.com>
2018-05-08 18:34:32 -0700

    Merge findAnyMSBuildWithCppPlatformToolset.ps1 into bootstrap.ps1

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -7,6 +7,7 @@ Set-StrictMode -Version Latest
 $scriptsDir = split-path -parent $script:MyInvocation.MyCommand.Definition
 
 $vcpkgRootDir = $scriptsDir
+$withVSPath = $withVSPath -replace "\\$" # Remove potential trailing backslash
 
 while (!($vcpkgRootDir -eq "") -and !(Test-Path "$vcpkgRootDir\.vcpkg-root"))
 {
@@ -24,7 +25,58 @@ if (!(Test-Path $vcpkgSourcesPath))
     return
 }
 
-$msbuildExeWithPlatformToolset = & $scriptsDir\findAnyMSBuildWithCppPlatformToolset.ps1 $withVSPath
+function findAnyMSBuildWithCppPlatformToolset([string]$withVSPath)
+{
+    $VisualStudioInstallationInstances = & $scriptsDir\findVisualStudioInstallationInstances.ps1
+    if ($VisualStudioInstallationInstances -eq $null)
+    {
+        throw "Could not find Visual Studio. VS2015 or VS2017 (with C++) needs to be installed."
+    }
+
+    Write-Verbose "VS Candidates:`n`r$([system.String]::Join([Environment]::NewLine, $VisualStudioInstallationInstances))"
+    foreach ($instanceCandidateWithEOL in $VisualStudioInstallationInstances)
+    {
+        $instanceCandidate = $instanceCandidateWithEOL -replace "<sol>::" -replace "::<eol>"
+        Write-Verbose "Inspecting: $instanceCandidate"
+        $split = $instanceCandidate -split "::"
+        # $preferenceWeight = $split[0]
+        # $releaseType = $split[1]
+        $version = $split[2]
+        $path = $split[3]
+
+        if ($withVSPath -ne "" -and $withVSPath -ne $path)
+        {
+            Write-Verbose "Skipping: $instanceCandidate"
+            continue
+        }
+
+        $majorVersion = $version.Substring(0,2);
+        if ($majorVersion -eq "15")
+        {
+            $VCFolder= "$path\VC\Tools\MSVC\"
+            if (Test-Path $VCFolder)
+            {
+                Write-Verbose "Picking: $instanceCandidate"
+                return "$path\MSBuild\15.0\Bin\MSBuild.exe", "v141"
+            }
+        }
+
+        if ($majorVersion -eq "14")
+        {
+            $clExe= "$path\VC\bin\cl.exe"
+            if (Test-Path $clExe)
+            {
+                Write-Verbose "Picking: $instanceCandidate"
+                $programFilesPath = getProgramFiles32bit
+                return "$programFilesPath\MSBuild\14.0\Bin\MSBuild.exe", "v140"
+            }
+        }
+    }
+
+    throw "Could not find MSBuild version with C++ support. VS2015 or VS2017 (with C++) needs to be installed."
+}
+
+$msbuildExeWithPlatformToolset = findAnyMSBuildWithCppPlatformToolset $withVSPath
 $msbuildExe = $msbuildExeWithPlatformToolset[0]
 $platformToolset = $msbuildExeWithPlatformToolset[1]
 $windowsSDK = & $scriptsDir\getWindowsSDK.ps1
